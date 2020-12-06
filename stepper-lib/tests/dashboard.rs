63f20f1a2483d18912cf8e8194a06c5bce196c4c
vms <michail.vms@gmail.com>
2020-12-06 15:48:57 +0300

    update

--- stepper-lib/tests/dashboard.rs
+++ stepper-lib/tests/dashboard.rs
@@ -14,34 +14,17 @@
  * limitations under the License.
  */
 
+use aqua_test_utils::call_vm;
 use aqua_test_utils::create_aqua_vm;
-use aqua_test_utils::set_variables_call_service;
-use aqua_test_utils::unit_call_service;
-use aqua_test_utils::{call_vm, echo_string_call_service};
 use aquamarine_vm::vec1::Vec1;
-use aquamarine_vm::HostExportedFunc;
 use aquamarine_vm::IValue;
-use stepper_lib::CallEvidencePath;
+use aquamarine_vm::{AquamarineVM, HostExportedFunc};
 
-use serde::Deserialize;
-use serde_json::json;
-
+use std::cell::RefCell;
+use std::collections::HashSet;
 use std::rc::Rc;
 
 type JValue = serde_json::Value;
-
-#[derive(Deserialize)]
-struct CallEvidencePathPrintable(pub CallEvidencePath);
-
-impl std::fmt::Display for CallEvidencePathPrintable {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        for state in self.0.iter() {
-            writeln!(f, "  {}", state)?;
-        }
-
-        Ok(())
-    }
-}
 
 fn parse_peers() -> Vec<String> {
     use csv::ReaderBuilder;
@@ -63,33 +46,32 @@ fn parse_peers() -> Vec<String> {
     return result;
 }
 
+fn into_hashset(peers: Vec<String>) -> HashSet<String> {
+    peers.into_iter().collect()
+}
+
 fn client_host_function(
     known_peers: Vec<String>,
     client_id: String,
     relay_id: String,
-) -> (HostExportedFunc, Rc<String>) {
-    let all_info = Rc::new(String::new());
+) -> (HostExportedFunc, Rc<RefCell<String>>) {
+    let all_info = Rc::new(RefCell::new(String::new()));
     let known_peers = JValue::Array(known_peers.iter().cloned().map(JValue::String).collect::<Vec<_>>());
     let client_id = JValue::String(client_id);
     let relay_id = JValue::String(relay_id);
 
     let to_ret_value = Box::new(
         move |service_name: &str, function_name: &str, arguments: Vec<&str>| -> JValue {
-            println!(
-                "to_ret_value of {} peer called with {} {} {:?}",
-                client_id, service_name, function_name, arguments
-            );
-
             match (service_name, function_name, arguments.as_slice()) {
                 ("", "load", &["relayId"]) => relay_id.clone(),
                 ("", "load", &["knownPeers"]) => known_peers.clone(),
                 ("", "load", &["clientId"]) => client_id.clone(),
-                ("event", "all_info", _) => JValue::Null,
                 _ => JValue::Null,
             }
         },
     );
 
+    let all_info_inner = all_info.clone();
     let host_function: HostExportedFunc = Box::new(move |_, args| -> Option<IValue> {
         let service_name = match &args[0] {
             IValue::String(str) => str,
@@ -106,9 +88,13 @@ fn client_host_function(
             _ => unreachable!(),
         };
 
-        let args: Vec<&str> = serde_json::from_str(function_args).unwrap();
-
-        let ret_value = to_ret_value(service_name.as_str(), function_name.as_str(), args);
+        let ret_value = match serde_json::from_str(function_args) {
+            Ok(args) => to_ret_value(service_name.as_str(), function_name.as_str(), args),
+            Err(_) => {
+                *all_info_inner.borrow_mut() = function_args.clone();
+                JValue::Null
+            }
+        };
 
         Some(IValue::Record(
             Vec1::new(vec![IValue::S32(0), IValue::String(ret_value.to_string())]).unwrap(),
@@ -119,7 +105,6 @@ fn client_host_function(
 }
 
 fn peer_host_function(
-    peer_id: String,
     known_peers: Vec<String>,
     blueprints: Vec<String>,
     modules: Vec<String>,
@@ -130,18 +115,11 @@ fn peer_host_function(
     let blueprints = JValue::Array(blueprints.into_iter().map(JValue::String).collect());
     let modules = JValue::Array(modules.into_iter().map(JValue::String).collect());
     let interfaces = JValue::Array(interfaces.into_iter().map(JValue::String).collect());
-    let identify = JValue::String(String::from("identify"));
+    let identify = JValue::String(ident.clone());
     let ident = JValue::String(ident);
 
     let to_ret_value = Box::new(
         move |service_name: &str, function_name: &str, arguments: Vec<&str>| -> JValue {
-            /*
-            println!(
-                "to_ret_value of {} peer called with {} {} {:?}",
-                peer_id, service_name, function_name, arguments
-            );
-             */
-
             match (service_name, function_name, arguments.as_slice()) {
                 ("op", "identity", _) => ident.clone(),
                 ("op", "identify", _) => identify.clone(),
@@ -188,13 +166,18 @@ fn create_peer_host_function(peer_id: String, known_peer_ids: Vec<String>) -> Ho
     let relay_ident = format!("{}_ident", peer_id);
 
     peer_host_function(
-        peer_id,
         known_peer_ids,
         relay_blueprints,
         relay_modules,
         relay_interfaces,
         relay_ident,
     )
+}
+
+struct AquaVMState {
+    vm: AquamarineVM,
+    peer_id: String,
+    prev_result: String,
 }
 
 #[test]
@@ -216,63 +199,185 @@ fn dashboard() {
     let mut known_peers = known_peer_ids
         .iter()
         .cloned()
-        .map(|id| create_aqua_vm(create_peer_host_function(id.clone(), known_peer_ids.clone()), id))
+        .map(|peer_id| {
+            let vm = create_aqua_vm(
+                create_peer_host_function(peer_id.clone(), known_peer_ids.clone()),
+                peer_id.clone(),
+            );
+            AquaVMState {
+                vm,
+                peer_id,
+                prev_result: String::new(),
+            }
+        })
         .collect::<Vec<_>>();
 
+    // -> client 1
     let client_1_res = call_vm!(client, client_id.clone(), script.clone(), "", "");
-    let client_1_res_path: CallEvidencePathPrintable = serde_json::from_str(&client_1_res.data).unwrap();
-
-    println!("-> client 1:");
-    // println!(" call evidence path:\n{}", client_1_res_path);
-    println!(" next peers pks: {:?}\n\n", client_1_res.next_peer_pks);
+    let next_peer_pks = into_hashset(client_1_res.next_peer_pks);
+    let mut all_peer_pks = into_hashset(known_peer_ids.clone());
+    all_peer_pks.insert(relay_id.clone());
+    assert_eq!(next_peer_pks, all_peer_pks);
 
     // client 1 -> relay 1
     let relay_1_res = call_vm!(relay, client_id.clone(), script.clone(), client_1_res.data.clone(), "");
-    let relay_1_res_path: CallEvidencePathPrintable = serde_json::from_str(&relay_1_res.data).unwrap();
+    let next_peer_pks = into_hashset(relay_1_res.next_peer_pks.clone());
+    all_peer_pks.remove(&relay_id);
+    all_peer_pks.insert(client_id.clone());
+    assert_eq!(next_peer_pks, all_peer_pks);
 
-    println!("client 1 -> relay 1");
-    // println!(" call evidence path:\n{}", relay_1_res_path);
-    println!(" next peers pks: {:?}\n\n", relay_1_res.next_peer_pks);
+    // relay 1 -> client 2
+    let client_2_res = call_vm!(
+        client,
+        client_id.clone(),
+        script.clone(),
+        client_1_res.data.clone(),
+        relay_1_res.data.clone()
+    );
+    assert!(client_2_res.next_peer_pks.is_empty());
+    assert_eq!(
+        *all_info.borrow(),
+        String::from(
+            r#"["relay_id","relay_id_ident",["relay_id_interface_0","relay_id_interface_1","relay_id_interface_2"],["relay_id_blueprint_0","relay_id_blueprint_1","relay_id_blueprint_2"],["relay_id_module_0","relay_id_module_1","relay_id_module_2"]]"#
+        )
+    );
 
-    // client 1 -> peers
-    let mut known_peers_1_results = Vec::new();
-    for (id, known_peer) in known_peers.iter_mut().enumerate() {
+    let mut relay_2_res = relay_1_res.clone();
+    let mut client_3_res = client_2_res.clone();
+
+    // peers 1 -> relay 2 -> client 3
+    for aqua_vm in known_peers.iter_mut() {
+        let prev_result = std::mem::replace(&mut aqua_vm.prev_result, String::new());
         let known_peer_res = call_vm!(
-            known_peer,
+            aqua_vm.vm,
             client_id.clone(),
             script.clone(),
-            client_1_res.data.clone(),
-            ""
+            prev_result,
+            client_1_res.data.clone()
         );
-        let known_peer_res_path: CallEvidencePathPrintable = serde_json::from_str(&known_peer_res.data).unwrap();
+        assert_eq!(known_peer_res.next_peer_pks, vec![relay_id.clone()]);
 
-        println!("client 1 -> {} ", &known_peer_ids[id]);
-        // println!(" call evidence path:\n{}", known_peer_res_path);
-        println!(" next peers pks: {:?}\n\n", known_peer_res.next_peer_pks);
+        aqua_vm.prev_result = known_peer_res.data;
 
-        known_peers_1_results.push(known_peer_res);
+        relay_2_res = call_vm!(
+            relay,
+            client_id.clone(),
+            script.clone(),
+            relay_2_res.data.clone(),
+            aqua_vm.prev_result.clone()
+        );
+        assert_eq!(relay_2_res.next_peer_pks, vec![client_id.clone()]);
+
+        client_3_res = call_vm!(
+            client,
+            client_id.clone(),
+            script.clone(),
+            client_3_res.data.clone(),
+            relay_2_res.data.clone()
+        );
+        assert!(client_3_res.next_peer_pks.is_empty());
+        assert_eq!(
+            *all_info.borrow(),
+            format!(
+                r#"["{peer_id}","{peer_id}_ident",["{peer_id}_interface_0","{peer_id}_interface_1","{peer_id}_interface_2"],["{peer_id}_blueprint_0","{peer_id}_blueprint_1","{peer_id}_blueprint_2"],["{peer_id}_module_0","{peer_id}_module_1","{peer_id}_module_2"]]"#,
+                peer_id = aqua_vm.peer_id
+            )
+        )
     }
 
-    // env_logger::init();
+    all_peer_pks.remove(&client_id);
+    all_peer_pks.insert(relay_id.clone());
 
-    // client 1 -> peers
-    // client 1 -> relay 1 -> peers
-    let mut known_peers_2_results = Vec::new();
-    for (id, known_peer) in known_peers.iter_mut().enumerate() {
+    let mut relay_3_res = relay_2_res.clone();
+    let mut client_4_res = client_3_res.clone();
+
+    // peers 2 -> relay 3 -> client 4
+    for aqua_vm in known_peers.iter_mut() {
+        let prev_result = std::mem::replace(&mut aqua_vm.prev_result, String::new());
         let known_peer_res = call_vm!(
-            known_peer,
+            aqua_vm.vm,
             client_id.clone(),
             script.clone(),
-            known_peers_1_results[id].data.clone(),
+            prev_result,
             relay_1_res.data.clone()
         );
-        let known_peer_res_path: CallEvidencePathPrintable = serde_json::from_str(&known_peer_res.data).unwrap();
+        all_peer_pks.remove(&aqua_vm.peer_id);
+        let next_peer_pks = into_hashset(known_peer_res.next_peer_pks.clone());
+        assert_eq!(next_peer_pks, all_peer_pks);
 
-        println!("client 1 -> peers");
-        println!("client 1 -> relay 1 -> {} ", &known_peer_ids[id]);
-        // println!(" call evidence path:\n{}", known_peer_res_path);
-        println!(" next peers pks: {:?}\n\n", known_peer_res.next_peer_pks);
+        all_peer_pks.insert(aqua_vm.peer_id.clone());
 
-        known_peers_2_results.push(known_peer_res);
+        aqua_vm.prev_result = known_peer_res.data;
+
+        relay_3_res = call_vm!(
+            relay,
+            client_id.clone(),
+            script.clone(),
+            relay_3_res.data.clone(),
+            aqua_vm.prev_result.clone()
+        );
+        assert_eq!(relay_3_res.next_peer_pks, vec![client_id.clone()]);
+
+        // client -> peers -> relay -> client
+        client_4_res = call_vm!(
+            client,
+            client_id.clone(),
+            script.clone(),
+            client_4_res.data.clone(),
+            relay_3_res.data.clone()
+        );
+        assert!(client_4_res.next_peer_pks.is_empty());
+        assert_eq!(
+            *all_info.borrow(),
+            format!(
+                r#"["{peer_id}","{peer_id}_ident",["{peer_id}_interface_0","{peer_id}_interface_1","{peer_id}_interface_2"],["{peer_id}_blueprint_0","{peer_id}_blueprint_1","{peer_id}_blueprint_2"],["{peer_id}_module_0","{peer_id}_module_1","{peer_id}_module_2"]]"#,
+                peer_id = aqua_vm.peer_id
+            )
+        )
+    }
+
+    let mut relay_4_res = relay_3_res.clone();
+    let mut client_5_res = client_4_res.clone();
+
+    // peers 2 -> peers 3 -> relay 4 -> client 5
+    for i in 0..known_peers.len() {
+        for j in 0..known_peers.len() {
+            if known_peers[i].peer_id == known_peers[j].peer_id {
+                continue;
+            }
+
+            let prev_data = known_peers[j].prev_result.clone();
+            let data = known_peers[i].prev_result.clone();
+            let known_peer_i_j_res = call_vm!(known_peers[j].vm, client_id.clone(), script.clone(), prev_data, data);
+            assert_eq!(known_peer_i_j_res.next_peer_pks, vec![relay_id.clone()]);
+
+            known_peers[j].prev_result = known_peer_i_j_res.data;
+
+            relay_4_res = call_vm!(
+                relay,
+                client_id.clone(),
+                script.clone(),
+                relay_4_res.data.clone(),
+                known_peers[j].prev_result.clone()
+            );
+            assert_eq!(relay_4_res.next_peer_pks, vec![client_id.clone()]);
+
+            // client -> peers -> relay -> client
+            client_5_res = call_vm!(
+                client,
+                client_id.clone(),
+                script.clone(),
+                client_5_res.data.clone(),
+                relay_4_res.data.clone()
+            );
+            assert!(client_5_res.next_peer_pks.is_empty());
+            assert_eq!(
+                *all_info.borrow(),
+                format!(
+                    r#"["{peer_id}","{peer_id}_ident",["{peer_id}_interface_0","{peer_id}_interface_1","{peer_id}_interface_2"],["{peer_id}_blueprint_0","{peer_id}_blueprint_1","{peer_id}_blueprint_2"],["{peer_id}_module_0","{peer_id}_module_1","{peer_id}_module_2"]]"#,
+                    peer_id = known_peers[j].peer_id
+                )
+            );
+        }
     }
 }
