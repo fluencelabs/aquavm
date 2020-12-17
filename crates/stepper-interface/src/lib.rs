3a63bf12f47c8918f0a57fafc568ea60c15b787a
vms <michail.vms@gmail.com>
2020-12-17 12:30:07 +0300

    add constructor of StepperOutcome from ivalues

--- crates/stepper-interface/src/lib.rs
+++ crates/stepper-interface/src/lib.rs
@@ -18,6 +18,7 @@ use fluence::fce;
 
 use serde::Deserialize;
 use serde::Serialize;
+use wasmer_wit::values::InterfaceValue as IValue;
 
 pub const STEPPER_SUCCESS: i32 = 0;
 
@@ -37,4 +38,71 @@ pub struct StepperOutcome {
 
     /// Public keys of peers that should receive data.
     pub next_peer_pks: Vec<String>,
+}
+
+impl StepperOutcome {
+    pub fn from_ivalues(mut ivalues: Vec<IValue>) -> Result<Self, String> {
+        const OUTCOME_FIELDS_COUNT: usize = 4;
+
+        let record_values = match ivalues.remove(0) {
+            IValue::Record(record_values) => record_values,
+            v => return Err(format!("expected record for StepperOutcome, got {:?}", v)),
+        };
+
+        let mut record_values = record_values.into_vec();
+        if record_values.len() != OUTCOME_FIELDS_COUNT {
+            return Err(format!(
+                "expected StepperOutcome struct with {} fields, got {:?}",
+                OUTCOME_FIELDS_COUNT, record_values
+            ));
+        }
+
+        let ret_code = match record_values.remove(0) {
+            IValue::S32(ret_code) => ret_code,
+            v => return Err(format!("expected i32 for ret_code, got {:?}", v)),
+        };
+
+        let error_message = match record_values.remove(0) {
+            IValue::String(str) => str,
+            v => return Err(format!("expected string for data, got {:?}", v)),
+        };
+
+        let data = match record_values.remove(0) {
+            IValue::Array(array) => {
+                let array: Result<Vec<_>, _> = array
+                    .into_iter()
+                    .map(|v| match v {
+                        IValue::U8(byte) => Ok(byte),
+                        v => Err(format!("expected a byte, got {:?}", v)),
+                    })
+                    .collect();
+                array?
+            }
+            v => return Err(format!("expected Vec<u8> for data, got {:?}", v)),
+        };
+
+        let next_peer_pks = match record_values.remove(0) {
+            IValue::Array(ar_values) => {
+                let array = ar_values
+                    .into_iter()
+                    .map(|v| match v {
+                        IValue::String(str) => Ok(str),
+                        v => Err(format!("expected string for next_peer_pks, got {:?}", v)),
+                    })
+                    .collect::<Result<Vec<String>, _>>()?;
+
+                Ok(array)
+            }
+            v => Err(format!("expected array for next_peer_pks, got {:?}", v)),
+        }?;
+
+        let outcome = Self {
+            ret_code,
+            error_message,
+            data,
+            next_peer_pks,
+        };
+
+        Ok(outcome)
+    }
 }
