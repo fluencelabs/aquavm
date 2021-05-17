286c6e589786b03d1d5ae815bbf9bbb1ac7d903f
Pavel Murygin <pavel.murygin@gmail.com>
2021-05-17 17:40:44 +0300

    Rename air interpreter to avm

--- avm/client/build_wasm.ps1
+++ avm/client/build_wasm.ps1
@@ -1,8 +1,8 @@
 
 New-Item -ItemType Directory -Force -Path ./wasm
-wasm-pack build ../../air-interpreter --no-typescript --release -d wasm
+wasm-pack build ../../air-interpreter --no-typescript --release -d ../avm/client/wasm
 
-$base64string = [Convert]::ToBase64String([IO.File]::ReadAllBytes('./wasm/air_interpreter_client_bg.wasm'))
+$base64string = [Convert]::ToBase64String([IO.File]::ReadAllBytes('./avm/client/wasm/air_interpreter_client_bg.wasm'))
 
 $data = "// auto-generated
 
