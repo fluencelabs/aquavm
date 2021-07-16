e4b0f57c5b43e73932e851c7ee0631b2c99e51e6
Pavel Murygin <pavel.murygin@gmail.com>
2021-07-16 22:28:52 +0300

    trying to automate

--- avm/client/build_wasm.sh
+++ avm/client/build_wasm.sh
@@ -14,3 +14,13 @@ cat << EOF > ./src/wasm.js
 
 module.exports = "$(base64 -w0 wasm/air_interpreter_client_bg.wasm)";
 EOF
+
+__wbg_callserviceimpl=$(cat wasm/air_interpreter_client_bg.js | grep __wbg_callserviceimpl\w+)
+__wbg_getcurrentpeeridimpl=$(cat wasm/air_interpreter_client_bg.js | grep __wbg_getcurrentpeeridimpl_\w+)
+
+cat << EOF > ./src/importObject.ts
+// auto-generated
+
+export const __wbg_callserviceimpl = '${__wbg_callserviceimpl}';
+export const __wbg_getcurrentpeeridimpl = '${__wbg_getcurrentpeeridimpl}';
+EOF
--- avm/client/src/importObject.ts
+++
@@ -1,2 +0,0 @@
-export const __wbg_callserviceimpl = '__wbg_callserviceimpl_34891090e13ace2e';
-export const __wbg_getcurrentpeeridimpl = '__wbg_getcurrentpeeridimpl_ecf9cb0901ce08ba';
