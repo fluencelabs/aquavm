852574451f8d4c4316cb0d85ab990c0ded746ad7
Pavel Murygin <pavel.murygin@gmail.com>
2021-07-16 22:39:09 +0300

    implement logic in powershell

--- avm/client/build_wasm.ps1
+++ avm/client/build_wasm.ps1
@@ -9,3 +9,17 @@ $data = "// auto-generated
 module.exports = `"${base64string}`"" 
 
 $data | Out-File "./src/wasm.js"
+
+$__wbg_callserviceimpl = Get-Content wasm/air_interpreter_client_bg.js | Select-String -Pattern __wbg_callserviceimpl\w+
+$__wbg_getcurrentpeeridimpl = Get-Content wasm/air_interpreter_client_bg.js | Select-String -Pattern __wbg_getcurrentpeeridimpl_\w+
+
+$__wbg_callserviceimpl = $__wbg_callserviceimpl.matches[0].value
+$__wbg_getcurrentpeeridimpl = $__wbg_getcurrentpeeridimpl.matches[0].value
+
+
+$data = "// auto-generated
+
+export const __wbg_callserviceimpl = '${__wbg_callserviceimpl}';
+export const __wbg_getcurrentpeeridimpl = '${__wbg_getcurrentpeeridimpl}';"
+
+$data | Out-File "./src/importObject.ts"
