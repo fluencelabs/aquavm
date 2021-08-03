0528eb485ae9092ed21548b19bb34900fcbaf26e
Pavel <Pavel.Murygin@gmail.com>
2021-08-03 23:13:47 +0300

    Build automation (#123)
    * preparation
    
    * trying to automate
    
    * fix
    
    * implement logic in powershell
    
    * fix
    
    * tmp
    
    * tmp
    
    * fix
    
    * remove debug stuff
    
    * newline in gitignore
    
    * Remove debug echos

--- avm/client/build_wasm.ps1
+++ avm/client/build_wasm.ps1
@@ -2,10 +2,24 @@
 New-Item -ItemType Directory -Force -Path ./wasm
 wasm-pack build ../../air-interpreter --no-typescript --release -d ../avm/client/wasm
 
-$base64string = [Convert]::ToBase64String([IO.File]::ReadAllBytes('./avm/client/wasm/air_interpreter_client_bg.wasm'))
+$base64string = [Convert]::ToBase64String([IO.File]::ReadAllBytes('./wasm/air_interpreter_client_bg.wasm'))
 
 $data = "// auto-generated
 
 module.exports = `"${base64string}`"" 
 
 $data | Out-File "./src/wasm.js"
+
+$__wbg_callserviceimpl = Get-Content wasm/air_interpreter_client_bg.js | Select-String -Pattern __wbg_callserviceimpl\w+
+$__wbg_getcurrentpeeridimpl = Get-Content wasm/air_interpreter_client_bg.js | Select-String -Pattern __wbg_getcurrentpeeridimpl_\w+
+
+$__wbg_callserviceimpl = $__wbg_callserviceimpl.matches[0].value
+$__wbg_getcurrentpeeridimpl = $__wbg_getcurrentpeeridimpl.matches[0].value
+
+
+$data = "// auto-generated
+
+export const __wbg_callserviceimpl = '${__wbg_callserviceimpl}';
+export const __wbg_getcurrentpeeridimpl = '${__wbg_getcurrentpeeridimpl}';"
+
+$data | Out-File "./src/importObject.ts"
