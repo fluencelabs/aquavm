6b1a3feef04dc6bf11c6e000afeac73719195b8f
vms <michail.vms@gmail.com>
2021-08-04 14:16:54 +0300

    fix air-parser tests

--- crates/air-parser/src/parser/tests.rs
+++ crates/air-parser/src/parser/tests.rs
@@ -797,6 +797,7 @@ fn fold_on_stream() {
     let instruction = parse(source_code);
     let expected = Instruction::FoldStream(FoldStream {
         stream_name: "$stream",
+        id: Rc::new("$stream_9".to_string()),
         iterator: "iterator",
         instruction: Rc::new(null()),
     });
