905bcdabedfb1b099614f9c82a58c2ead3ff3471
vms <michail.vms@gmail.com>
2021-08-17 21:52:45 +0300

    fix bug with ap streams

--- air/src/execution_step/trace_handler/merger/ap_merger.rs
+++ air/src/execution_step/trace_handler/merger/ap_merger.rs
@@ -34,8 +34,7 @@ pub(crate) fn try_merge_next_state_as_ap(data_keeper: &mut DataKeeper) -> MergeR
 
     let ap = match (prev_state, current_state) {
         (Some(Ap(prev_ap)), _) => prev_ap,
-        // this special case is needed to merge stream generation in a right way
-        (None, Some(Ap(current_ap))) => current_ap,
+        (None, Some(Ap(_))) => return Ok(MergerApResult::Empty),
         (None, None) => return Ok(MergerApResult::Empty),
         (prev_state, current_state) => return Err(MergeError::incompatible_states(prev_state, current_state, "ap")),
     };
