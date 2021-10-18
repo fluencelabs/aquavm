29a727ec5391c7b2723b28e8da90aa24721ddb3a
vms <michail.vms@gmail.com>
2021-10-18 17:11:32 +0300

    fux bench

--- air/benches/call_benchmark.rs
+++ air/benches/call_benchmark.rs
@@ -1,8 +1,4 @@
-use air_test_utils::create_avm;
-use air_test_utils::unit_call_service;
-use air_test_utils::AVMError;
-use air_test_utils::InterpreterOutcome;
-use air_test_utils::AVM;
+use air_test_utils::prelude::*;
 
 use criterion::criterion_group;
 use criterion::criterion_main;
@@ -10,7 +6,7 @@ use criterion::Criterion;
 
 use std::cell::RefCell;
 
-thread_local!(static VM: RefCell<AVM> = RefCell::new(create_avm(unit_call_service(), "test_peer_id")));
+thread_local!(static VM: RefCell<TestRunner> = RefCell::new(create_avm(unit_call_service(), "test_peer_id")));
 thread_local!(static SCRIPT: String = String::from(
         r#"
             (call "test_peer_id" ("local_service_id" "local_fn_name") [] result_name)
@@ -18,8 +14,8 @@ thread_local!(static SCRIPT: String = String::from(
     )
 );
 
-fn current_peer_id_call() -> Result<InterpreterOutcome, AVMError> {
-    VM.with(|vm| SCRIPT.with(|script| vm.borrow_mut().call_with_prev_data("", script.clone(), "", "")))
+fn current_peer_id_call() -> Result<RawAVMOutcome, String> {
+    VM.with(|vm| SCRIPT.with(|script| vm.borrow_mut().call(script, "", "", "")))
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
