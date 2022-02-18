39da1c98bbc34f22653003e1a6b1548c34d1a691
Valery Antopol <valery.antopol@gmail.com>
2022-02-18 19:04:40 +0300

    Use js marine runtime instead of wasm-bindgen (#209)

--- avm/client/build_wasm.ps1
+++
@@ -1,7 +0,0 @@
-
-New-Item -ItemType Directory -Force -Path ./wasm
-wasm-pack build ../../air-interpreter --no-typescript --release -d ../avm/client/wasm
-
-New-Item -ItemType Directory -Force -Path ./dist
-cp wasm/air_interpreter_client_bg.wasm dist/avm.wasm
-cp dist/avm.wasm src/__test__/
--- avm/client/build_wasm.sh
+++ avm/client/build_wasm.sh
@@ -1,14 +1,9 @@
 #!/bin/sh
-
-## requires wasm-pack
-## > curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
-
 (
-    cd ../..;
-    mkdir -p ./avm/client/wasm || exit;
-    wasm-pack build ./air-interpreter --no-typescript --release -d ../avm/client/wasm
+  cd ../../air-interpreter;
+  marine build  --release --features marine
 )
 
 mkdir -p ./dist/
-cp wasm/air_interpreter_client_bg.wasm dist/avm.wasm
-cp dist/avm.wasm src/__test__/
+cp ../../target/wasm32-wasi/release/air_interpreter_server.wasm ./dist/avm.wasm
+
