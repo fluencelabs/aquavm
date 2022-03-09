8a92ea01444b36a04ba7ece3a01eab92c270b615
vms <michail.vms@gmail.com>
2022-03-10 01:06:21 +0300

    add test

--- air/tests/test_module/integration/recursive_streams.rs
+++ air/tests/test_module/integration/recursive_streams.rs
@@ -374,3 +374,77 @@ fn recursive_stream_inner_fold() {
 
     assert_eq!(actual_fold_lores_count, stop_request_id);
 }
+
+#[test]
+fn recursive_stream_fold_with_n_service_call() {
+    let vm_peer_id = "vm_peer_id_1";
+
+    let request_id = std::cell::Cell::new(0);
+    let stop_request_id = 10;
+    let give_n_results_and_then_stop: CallServiceClosure = Box::new(move |_params| {
+        let uncelled_request_id = request_id.get();
+
+        let result = if uncelled_request_id >= stop_request_id {
+            CallServiceResult::ok(json!("no"))
+        } else {
+            CallServiceResult::ok(json!("yes"))
+        };
+
+        request_id.set(uncelled_request_id + 1);
+        result
+    });
+
+    let mut vm = create_avm(give_n_results_and_then_stop, vm_peer_id);
+
+    let script = f!(r#"
+    (xor
+     (seq
+      (seq
+       (call %init_peer_id% ("getDataSrv" "-relay-") [] -relay-)
+       (new $loop
+        (new $result
+         (seq
+          (seq
+           (ap "yes" $loop)
+           (fold $loop l
+            (seq
+             (seq
+              (xor
+               (match l "yes"
+                (xor
+                 (call %init_peer_id% ("yesno" "get") [] $loop)
+                 (call %init_peer_id% ("errorHandlingSrv" "error") [%last_error% 1])
+                )
+               )
+               (null)
+              )
+              (ap "success" $result)
+             )
+             (next l)
+            )
+           )
+          )
+          (call %init_peer_id% ("op" "identity") [$result] result-fix)
+         )
+        )
+       )
+      )
+      (xor
+       (call %init_peer_id% ("callbackSrv" "response") [result-fix])
+       (call %init_peer_id% ("errorHandlingSrv" "error") [%last_error% 2])
+      )
+     )
+     (call %init_peer_id% ("errorHandlingSrv" "error") [%last_error% 3])
+    )
+    "#);
+
+    let result = checked_call_vm!(vm, vm_peer_id, &script, "", "");
+    let actual_trace = trace_from_result(&result);
+    let actual_fold_state = match &actual_trace[2] {
+        ExecutedState::Fold(fold_result) => fold_result,
+        _ => panic!("2nd state should be fold"),
+    };
+    let expected_fold_lores = stop_request_id + 1;
+
+    assert_eq!(actual_fold_state.lore.len(), expected_fold_lores);
+}
