f69cc7316634ccc7193891f4b2d73cd997423a6c
Ivan Boldyrev <ivan@fluence.one>
2022-09-01 19:26:07 +0400

    Make some code cleaner and safe

--- crates/air-lib/lambda/parser/src/parser/lambda_parser.rs
+++ crates/air-lib/lambda/parser/src/parser/lambda_parser.rs
@@ -23,6 +23,8 @@ use crate::ValueAccessor;
 
 use va_lambda::LambdaParser;
 
+use std::convert::TryFrom;
+
 // Caching parser to cache internal regexes, which are expensive to instantiate
 // See also https://github.com/lalrpop/lalrpop/issues/269
 thread_local!(static PARSER: LambdaParser = LambdaParser::new());
@@ -42,11 +44,6 @@ pub fn parse(lambda: &str) -> LambdaParserResult<'_, LambdaAST> {
     })
 }
 
-fn try_to_lambda(accessors: Vec<ValueAccessor>) -> LambdaParserResult<'_, LambdaAST> {
-    if accessors.is_empty() {
-        return Err(LambdaParserError::EmptyLambda);
-    }
-
-    let ast = unsafe { LambdaAST::new_unchecked(accessors) };
-    Ok(ast)
+fn try_to_lambda(accessors: Vec<ValueAccessor>) -> LambdaParserResult<'_, LambdaAST<'_>> {
+    LambdaAST::try_from(accessors).or(Err(LambdaParserError::EmptyLambda))
 }
