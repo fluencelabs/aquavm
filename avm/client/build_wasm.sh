1e33f412ed4dee8c661f1856a16a9d0584a6c2e7
Anatoly Laskaris <github_me@nahsi.dev>
2022-09-16 08:42:12 +0300

    Fix build wasm script when running as user

--- avm/client/build_wasm.sh
+++ avm/client/build_wasm.sh
@@ -1,13 +1,14 @@
 #! /usr/bin/env bash
 
-if [[ -f dist/avm.wasm ]]; then
+# if running in CI avm.wasm has been built already
+if [[ -z $GITHUB_SHA ]] && [[ -f dist/avm.wasm ]]; then
   echo "air-interpreter wasm binary already present"
   exit 0
 else
-  cd ../../air-interpreter
-  marine build  --release --features marine
-  cd -
-
-  mkdir -p ./dist/
-  cp ../../target/wasm32-wasi/release/air_interpreter_server.wasm ./dist/avm.wasm
+  (
+    cd ../../air-interpreter
+    marine build --release --features marine
+  )
+  mkdir -p dist/
+  cp ../../target/wasm32-wasi/release/air_interpreter_server.wasm dist/avm.wasm
 fi
