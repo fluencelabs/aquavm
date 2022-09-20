61cc3ffc2c995a39e1c84dea1b204f71d03f8c29
Anatoly Laskaris <github_me@nahsi.dev>
2022-09-20 18:25:55 +0300

    ci: Add Aquavm E2E (#324)

--- avm/client/build_wasm.sh
+++ avm/client/build_wasm.sh
@@ -1,9 +1,14 @@
-#!/bin/sh
-(
-  cd ../../air-interpreter;
-  marine build  --release --features marine
-)
+#! /usr/bin/env bash
 
-mkdir -p ./dist/
-cp ../../target/wasm32-wasi/release/air_interpreter_server.wasm ./dist/avm.wasm
-
+# if running in CI avm.wasm has been built already
+if [[ ! -z $GITHUB_SHA ]] && [[ -f dist/avm.wasm ]]; then
+  echo "air-interpreter wasm binary already present"
+  exit 0
+else
+  (
+    cd ../../air-interpreter
+    marine build --release --features marine
+  )
+  mkdir -p dist/
+  cp ../../target/wasm32-wasi/release/air_interpreter_server.wasm dist/avm.wasm
+fi
