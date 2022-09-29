c58ed5dd5437b7b4b530318546ae885f266e118f
Ivan Boldyrev <ivan@fluence.one>
2022-09-29 19:40:10 +0400

    Fix issue_177

--- air/tests/test_module/issues/scripts/issue_177.clj
+++ air/tests/test_module/issues/scripts/issue_177.clj
@@ -5,45 +5,48 @@
    (new $res
     (seq
      (seq
-      (seq
-       (call -relay- ("op" "noop") [])
-       (xor
-        (seq
-         (call "12D3KooWSD5PToNiLQwKDXsu8JSysCwUt8BVUJEqCHcDe7P5h45e" ("op" "string_to_b58") ["12D3KooWSD5PToNiLQwKDXsu8JSysCwUt8BVUJEqCHcDe7P5h45e"] k)
-         (par
-          (seq
-           (call "12D3KooWSD5PToNiLQwKDXsu8JSysCwUt8BVUJEqCHcDe7P5h45e" ("kad" "neighborhood") [k [] []] nodes)
-           (call %init_peer_id% ("op" "noop") [])
+      (call -relay- ("op" "noop") [])
+      (xor
+       (seq
+        (call "12D3KooWSD5PToNiLQwKDXsu8JSysCwUt8BVUJEqCHcDe7P5h45e" ("op" "string_to_b58") ["12D3KooWSD5PToNiLQwKDXsu8JSysCwUt8BVUJEqCHcDe7P5h45e"] k)
+        (par
+         (seq
+          (call "12D3KooWSD5PToNiLQwKDXsu8JSysCwUt8BVUJEqCHcDe7P5h45e" ("kad" "neighborhood") [k [] []] nodes)
+          (call %init_peer_id% ("op" "noop") [])
+          )
+         (seq
+          (fold nodes n
+           (par
+            (seq
+             (xor
+              (call n ("peer" "timestamp_ms") [] $res)
+              (seq
+               (call -relay- ("op" "noop") [])
+               (call %init_peer_id% ("errorHandlingSrv" "error") [%last_error% 1])
+               )
+              )
+              (call %init_peer_id% ("op" "noop") [])
+              )
+            (next n)
+            )
            )
-          (seq
-           (fold nodes n
-                 (par
-                  (seq
-                   (xor
-                    (call n ("peer" "timestamp_ms") [] $res)
-                    (seq
-                     (call -relay- ("op" "noop") [])
-                     (call %init_peer_id% ("errorHandlingSrv" "error") [%last_error% 1])
-                     )
-                    )
-                   (call %init_peer_id% ("op" "noop") [])
-                   )
-                  (next n)
-                  )
-                 )
-           (call %init_peer_id% ("op" "noop") [])
-           )
+          (call %init_peer_id% ("op" "noop") [])
           )
          )
-        (call %init_peer_id% ("errorHandlingSrv" "error") [%last_error% 2])
         )
-       )
-      (par
-       (call %init_peer_id% ("op" "identity") [$res.$.[19]!])
-       (call %init_peer_id% ("peer" "timeout") [1000 "timeout"])
+       (call %init_peer_id% ("errorHandlingSrv" "error") [%last_error% 2])
        )
       )
-     (call %init_peer_id% ("op" "identity") [$res] res-fix)
+     (seq
+      (canon %init_peer_id% $res #res)
+      (seq
+       (par
+        (call %init_peer_id% ("op" "identity") [#res.$.[19]!])
+        (call %init_peer_id% ("peer" "timeout") [1000 "timeout"])
+        )
+       (call %init_peer_id% ("op" "identity") [#res] res-fix)
+       )
+      )
      )
     )
    )
