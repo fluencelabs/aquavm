4cac95695b49aa8f003c5756336ac75f791cbffb
vms <michail.vms@gmail.com>
2022-10-10 15:29:25 +0300

    allow non used streams in validator

--- air/tests/test_module/instructions/canon.rs
+++ air/tests/test_module/instructions/canon.rs
@@ -238,6 +238,21 @@ fn canon_empty_stream() {
 }
 
 #[test]
+fn canon_empty_not_writable_stream() {
+    let peer_id_1 = "peer_id_1";
+    let mut vm_1 = create_avm(echo_call_service(), peer_id_1);
+
+    let script = f!(r#"
+        (canon "{peer_id_1}" $stream #canon_stream)
+    "#);
+
+    let result = checked_call_vm!(vm_1, <_>::default(), &script, "", "");
+    let actual_trace = trace_from_result(&result);
+    let expected_trace = vec![executed_state::canon(vec![])];
+    assert_eq!(actual_trace, expected_trace);
+}
+
+#[test]
 fn canon_over_later_defined_stream() {
     let vm_peer_id_1 = "vm_peer_id_1";
     let mut peer_vm_1 = create_avm(echo_call_service(), vm_peer_id_1);
