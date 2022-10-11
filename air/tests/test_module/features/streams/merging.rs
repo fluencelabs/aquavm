fab0034d5cf21fa8f7e63ebc45241c6504f213a9
Valery Antopol <valery.antopol@gmail.com>
2022-10-12 02:51:36 +0300

    add permutations test

--- air/tests/test_module/features/streams/merging.rs
+++ air/tests/test_module/features/streams/merging.rs
@@ -125,3 +125,145 @@ fn merging_fold_iterations_extensively() {
 
     assert_eq!(last_fold.lore.len(), 18);
 }
+
+#[test]
+fn merging_fold_iterations_extensively_2() {
+    let script = r#"
+        (seq
+            (seq
+                (call "client" ("get" "data") [] permutations) ; ok = [["p1",[[["p1",1],["p2",2],["p3",3]],[["p1",4],["p3",5],["p2",6]]]],["p2",[[["p2",7],["p1",8],["p3",9]],[["p2",10],["p3",11],["p1",12]]]],["p3",[[["p3",13],["p1",14],["p2",15]],[["p3",16],["p2",17],["p1",18]]]]]
+                (seq
+                    (seq
+                        (fold permutations pair
+                            (seq
+                                (null)
+                                (seq
+                                    (fold pair.$.[1] pid-num-arr
+                                        (seq
+                                            (seq
+                                                (call pair.$.[0] ("op" "noop") []) ; ok = null
+                                                (ap pid-num-arr $pid-num-arrs)
+                                            )
+                                            (seq
+                                                (null)
+                                                (next pid-num-arr)
+                                            )
+                                        )
+                                    )
+                                    (next pair)
+                                )
+                            )
+                        )
+                        (call "p1" ("test" "print") [$pid-num-arrs]) ; behaviour = echo
+                    )
+                    (seq
+                        (seq
+                            (canon "p1" $pid-num-arrs #pid-num-arrs)
+                            (call "p1" ("test" "print") [$pid-num-arrs]) ; behaviour = echo
+                        )
+                        (new $result
+                            (fold $pid-num-arrs pid-num-arr
+                                (seq
+                                    (seq
+                                        (call "p1" ("test" "print") [pid-num-arr]) ; behaviour = echo
+                                        (fold pid-num-arr pid-num
+                                            (seq
+                                                (seq
+                                                    (null)
+                                                    (seq
+                                                        (call pid-num.$.[0] ("op" "noop") []) ; ok = null
+                                                        (ap pid-num.$.[1] $result)
+                                                    )
+                                                )
+                                                (seq
+                                                    (seq
+                                                        (canon pid-num.$.[0] $result #mon_res)
+                                                        (call pid-num.$.[0] ("test" "print") [#mon_res]) ; behaviour = echo
+                                                    )
+                                                    (next pid-num)
+                                                )
+                                            )
+                                        )
+                                    )
+                                    (seq
+                                        (seq
+                                            (canon "p1" $result #mon_res)
+                                            (call "p1" ("test" "print") [#mon_res]) ; behaviour = echo
+                                        )
+                                        (xor
+                                            (match #mon_res.length 18
+                                                (call "p1" ("test" "print") [#mon_res.length]) ; behaviour = echo
+                                            )
+                                            (seq
+                                                (call "p1" ("test" "print") ["not enought length"]) ; behaviour = echo
+                                                (next pid-num-arr)
+                                            )
+                                        )
+                                    )
+                                )
+                            )
+                        )
+                    )
+                )
+            )
+            (seq
+                (call "p1" ("op" "noop") ["final p1"]) ; behaviour = echo
+                (seq
+                    (canon "client" $result #end_result)
+                    (call "p1" ("return" "") [#end_result]) ; behaviour = echo
+                )
+            )
+        )
+                "#;
+
+    let engine = TestExecutor::new(
+        TestRunParameters::from_init_peer_id("client"),
+        vec![],
+        vec!["relay", "p1", "p2", "p3"].into_iter().map(Into::into),
+        &script,
+    )
+        .unwrap();
+
+    let mut queue = std::collections::vec_deque::VecDeque::new();
+    let mut p1_outcomes = Vec::<RawAVMOutcome>::new();
+    queue.push_back("client".to_string());
+
+    while !queue.is_empty() {
+        let peer = queue.pop_front().unwrap();
+        if let Some(outcomes) = engine.execution_iter(peer.as_str()) {
+            for outcome in outcomes {
+                assert_eq!(outcome.ret_code, 0, "{:?}", outcome);
+                //if outcome.next_peer_pks.len() > 1 {
+                println!("{} next peers: {:?}", peer, &outcome.next_peer_pks);
+                print_trace(&outcome, &peer);
+                //}
+
+                for peer in &outcome.next_peer_pks {
+                    if !queue.contains(peer) {
+                        queue.push_back(peer.clone());
+                    }
+                }
+
+                if peer == "p1" {
+                    p1_outcomes.push(outcome);
+                }
+            }
+        } else {
+            println!("peer: {}, no executions", peer);
+        }
+    }
+
+    let last_p1_data = p1_outcomes.last().unwrap();
+    let last_p1_trace = trace_from_result(last_p1_data);
+    let last_fold = last_p1_trace
+        .iter()
+        .filter_map(|state| match state {
+            ExecutedState::Fold(fold_result) => Some(fold_result),
+            _ => None,
+        })
+        .last()
+        .unwrap();
+
+    assert_eq!(last_fold.lore.len(), 6);
+}
+
