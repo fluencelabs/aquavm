36fc2a758a6668c2b1bc2ef5ba85afae39aade2e
Ivan Boldyrev <ivan@fluence.one>
2023-01-30 13:34:45 +0100

    Use func name for formatting

--- tools/cli/performance_metering/performance_metering/trace.py
+++ tools/cli/performance_metering/performance_metering/trace.py
@@ -24,23 +24,37 @@ class TraceRecord:
     """A parsed trace record."""
 
     span: str
+    target: str
+    spans: list
+    nested: dict
     execution_time: datetime.timedelta
     raw_time: str
 
-    def __init__(self, span, raw_time):
+    def __init__(self, span, target, raw_time, spans):
         """Create a TraceRecord instance."""
         self.span = span
         self.execution_time = parse_trace_timedelta(raw_time)
         self.raw_time = raw_time
+        self.spans = spans
+        self.nested = {}
+
+    def get_func_name(self) -> str:
+        """Return qualified function name."""
+        if self.target is None:
+            return self.span
+        else:
+            return "{}::{}".format(self.target, self.span)
 
 
 def parse_if_interesting(raw_rec: dict) -> Optional[TraceRecord]:
     """If trace is interesting, parse it; return None otherwise."""
     try:
         if raw_rec["fields"]["message"] == "close":
-            span = raw_rec["span"]["name"]
+            span = raw_rec["span"].get("name", "ERROR_missing_span.name")
+            target = raw_rec.get("target", None),
             time = raw_rec["fields"]["time.busy"]
-            return TraceRecord(span, time)
+            spans = [sp["name"] for sp in raw_rec.get("spans", [])]
+            return TraceRecord(span, target, time, spans)
     except KeyError:
         return None
     return None
@@ -52,7 +66,7 @@ def combine_traces(traces: list[TraceRecord], repeat: int):
     combined = defaultdict(datetime.timedelta)
 
     for trace in traces:
-        combined[trace.span] += trace.execution_time
+        combined[trace.get_func_name()] += trace.execution_time
 
     return {
         span: format_timedelta(time / repeat)
