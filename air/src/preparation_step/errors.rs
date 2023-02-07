3f87dea7263425cdb0bb75df3c2b1686ab43dde6
vms <michail.vms@gmail.com>
2023-02-07 15:34:49 +0300

    further refactoring

--- air/src/preparation_step/errors.rs
+++ air/src/preparation_step/errors.rs
@@ -16,6 +16,7 @@
 
 use crate::ToErrorCode;
 use air_interpreter_data::data_version;
+use air_interpreter_data::Versions;
 
 use serde_json::Error as SerdeJsonError;
 use strum::IntoEnumIterator;
@@ -33,20 +34,41 @@ pub enum PreparationError {
 
     /// Errors occurred on executed trace deserialization.
     #[error(
-        "an error occurred while executed trace deserialization: {0:?}.\n\
-        Interpreter of {} version is used and it's expecting data of {} version.\n\
-        Data: {1:?}",
+        "an error occurred while data deserialization: {error:?}.\n\
+        Your AquaVM's version is {} and it's expecting data of {} version,\
+        it's failed to get version of AquaVM produced this data.\n\
+        data: {data:?}",
         super::interpreter_version(),
         data_version()
     )]
-    DataDeFailed(SerdeJsonError, Vec<u8>),
+    DataDeFailed { data: Vec<u8>, error: SerdeJsonError },
+
+    /// Errors occurred on executed trace deserialization.
+    #[error(
+        "an error occurred while data deserialization: {error:?}.\n\
+        Your AquaVM's version is {} and it's expecting data of {} version.\n\
+        Supplied data version is {}, it's produced by AquaVM of {} version.\n\
+        Data: {data:?}",
+        super::interpreter_version(),
+        data_version(),
+        versions.data_version,
+        versions.interpreter_version,
+    )]
+    DataDeFailedWithVersions {
+        data: Vec<u8>,
+        error: SerdeJsonError,
+        versions: Versions,
+    },
 
     /// Error occurred on call results deserialization.
     #[error(
-        "error occurred while deserialize call results: {0:?}.\n\
-    Call results: {1:?}"
+        "error occurred while deserialize call results: {error:?}.\n\
+    Call results: {call_results:?}"
     )]
-    CallResultsDeFailed(SerdeJsonError, Vec<u8>),
+    CallResultsDeFailed {
+        call_results: Vec<u8>,
+        error: SerdeJsonError,
+    },
 
     /// Error occurred when a version of interpreter produced supplied data is less then minimal.
     #[error("supplied data was produced by `{actual_version}` version of interpreter, but minimum `{required_version}` version is required")]
@@ -60,5 +82,26 @@ impl ToErrorCode for PreparationError {
     fn to_error_code(&self) -> i64 {
         use crate::utils::PREPARATION_ERROR_START_ID;
         crate::generate_to_error_code!(self, PreparationError, PREPARATION_ERROR_START_ID)
+    }
+}
+
+impl PreparationError {
+    pub fn data_de_failed(data: Vec<u8>, error: SerdeJsonError) -> Self {
+        Self::DataDeFailed { data, error }
+    }
+
+    pub fn data_de_failed_with_versions(data: Vec<u8>, error: SerdeJsonError, versions: Versions) -> Self {
+        Self::DataDeFailedWithVersions { data, error, versions }
+    }
+
+    pub fn call_results_de_failed(call_results: Vec<u8>, error: SerdeJsonError) -> Self {
+        Self::CallResultsDeFailed { call_results, error }
+    }
+
+    pub fn unsupported_interpreter_version(actual_version: semver::Version, required_version: semver::Version) -> Self {
+        Self::UnsupportedInterpreterVersion {
+            actual_version,
+            required_version,
+        }
     }
 }
