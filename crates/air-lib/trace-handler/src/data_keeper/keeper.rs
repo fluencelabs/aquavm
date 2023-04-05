658daf1d3f6e733c15a21afc40ddf468ed745d43
raftedproc <71657594+raftedproc@users.noreply.github.com>
2023-04-05 11:50:15 +0300

    feat(trace-handler): TracePos becomes a wrapper for u32 alias [fixes VM-267] (#544)

--- crates/air-lib/trace-handler/src/data_keeper/keeper.rs
+++ crates/air-lib/trace-handler/src/data_keeper/keeper.rs
@@ -21,6 +21,8 @@ use crate::TracePos;
 
 use bimap::BiHashMap;
 
+use std::convert::TryInto;
+
 /// Keeps all necessary data for merging.
 #[derive(Debug, Default, PartialEq)]
 pub(crate) struct DataKeeper {
@@ -50,7 +52,7 @@ impl DataKeeper {
     }
 
     pub(crate) fn result_trace_next_pos(&self) -> TracePos {
-        self.result_trace.len().into()
+        self.result_trace.len().try_into().expect("try_into() TracePos failed.")
     }
 
     pub(crate) fn prev_slider(&self) -> &TraceSlider {
