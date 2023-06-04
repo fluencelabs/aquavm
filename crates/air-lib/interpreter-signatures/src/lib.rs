252d43b348356719cb217b15f3eba873ecc8dab1
Ivan Boldyrev <ivan@fluence.one>
2023-06-04 20:06:31 +0700

    feat(air-interpreter-signatures): use (de)serialize_with (#605)
    * feat(air-interpreter-signatures): use (de)serialize_with
    
    Wrapper types do not hold strings anymore, but use `(de)serialize_with`
    serde attributes to parse keys and signatures during parse time.
    
    `PublicKey` and `Signature` traits implement `Deref` trait that returns
    the inner value.

--- crates/air-lib/interpreter-signatures/src/lib.rs
+++ crates/air-lib/interpreter-signatures/src/lib.rs
@@ -26,6 +26,8 @@
     unreachable_patterns
 )]
 
+mod sede;
+
 use air_interpreter_cid::CID;
 use fluence_keypair::error::SigningError;
 use fluence_keypair::KeyPair;
@@ -35,43 +37,99 @@ use serde::{Deserialize, Serialize};
 use std::borrow::Borrow;
 use std::collections::HashMap;
 use std::hash::Hash;
+use std::ops::Deref;
 
-/// An opaque serializable representation of public key.
+/// An opaque serializable representation of a public key.
 ///
-/// It can be string or binary, you shouldn't care about it unless you change serialization format.
-// surrent implementation uses string as it is used as a key in a JSON map
-#[derive(Debug, Hash, Clone, Eq, PartialEq, PartialOrd, Deserialize, Serialize)]
-#[serde(transparent)]
-pub struct PublicKey(Box<str>);
+/// It can be a string or a binary, you shouldn't care about it unless you change serialization format.
+// surrent implementation serializes to string as it is used as a key in a JSON map
+#[derive(Debug, Clone, PartialEq, Eq, Deserialize, Serialize)]
+pub struct PublicKey(
+    #[serde(
+        deserialize_with = "sede::b58_to_public_key",
+        serialize_with = "sede::public_key_to_b58"
+    )]
+    fluence_keypair::PublicKey,
+);
+
+impl PublicKey {
+    pub fn verify<T: Serialize + ?Sized>(
+        &self,
+        value: &T,
+        signature: &fluence_keypair::Signature,
+    ) -> Result<(), fluence_keypair::error::VerificationError> {
+        let pk = &**self;
+
+        let serialized_value =
+            serde_json::to_vec(value).expect("default serialization shouldn't fail");
+
+        pk.verify(&serialized_value, signature)
+    }
+}
+
+impl Deref for PublicKey {
+    type Target = fluence_keypair::PublicKey;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl Hash for PublicKey {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.0.to_vec().hash(state);
+    }
+}
 
 impl From<fluence_keypair::PublicKey> for PublicKey {
     fn from(value: fluence_keypair::PublicKey) -> Self {
-        Self(bs58::encode(&value.to_vec()).into_string().into())
+        Self(value)
     }
 }
 
 /// An opaque serializable representation of signature key.
 ///
 /// It can be string or binary, you shouldn't care about it unless you change serialization format.
-// surrent implementation uses string as more compact in JSON representation than number array
-#[derive(Debug, Hash, Clone, Eq, PartialEq, PartialOrd, Deserialize, Serialize)]
+// surrent implementation serializes string as more compact in JSON representation than number array
+#[derive(Debug, Clone, Eq, PartialEq, Deserialize, Serialize)]
 #[serde(transparent)]
-pub struct Signature(Box<str>);
+pub struct Signature(
+    #[serde(
+        deserialize_with = "sede::b58_to_signature",
+        serialize_with = "sede::signature_to_b58"
+    )]
+    fluence_keypair::Signature,
+);
 
 impl Signature {
     fn new(signature: fluence_keypair::Signature) -> Self {
-        signature.into()
+        Self(signature)
+    }
+}
+
+impl Deref for Signature {
+    type Target = fluence_keypair::Signature;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
     }
 }
 
 impl From<fluence_keypair::Signature> for Signature {
     fn from(value: fluence_keypair::Signature) -> Self {
-        Self(bs58::encode(value.to_vec()).into_string().into())
+        Self(value)
+    }
+}
+
+impl From<Signature> for fluence_keypair::Signature {
+    fn from(value: Signature) -> Self {
+        value.0
     }
 }
 
 #[derive(Debug, Default)]
 pub struct SignatureTracker {
+    // from peer id to CID strings
     peer_to_cids: HashMap<Box<str>, Vec<Box<str>>>,
 }
 
