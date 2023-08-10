b3a622970fdac898074e4e34dd3f6405eba15fc4
Ivan Boldyrev <ivan@fluence.one>
2023-08-10 15:33:51 +0400

    Last fixes...

--- tools/wasm/air-near-contract/build.sh
+++ tools/wasm/air-near-contract/build.sh
@@ -3,4 +3,4 @@
 echo ">> Building contract"
 
 rustup target add wasm32-unknown-unknown
-cargo build --all --target wasm32-unknown-unknown --release
+cargo build --target wasm32-unknown-unknown --release
