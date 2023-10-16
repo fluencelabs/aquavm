e4649e5e7b6355944a7740ee272659269224ffc7
Ivan Boldyrev <ivan@fluence.one>
2023-10-16 18:12:15 +0400

    feat(interpreter-cid,interpreter-data)!: Support for mulitple hash types in CID verification
    Support mulitple hashing algorithms for CID verification.

--- crates/air-lib/interpreter-cid/src/lib.rs
+++ crates/air-lib/interpreter-cid/src/lib.rs
@@ -26,6 +26,10 @@
     unreachable_patterns
 )]
 
+mod verify;
+
+pub use crate::verify::{verify_value, CidVerificationError};
+
 use serde::Deserialize;
 use serde::Serialize;
 
@@ -37,8 +41,11 @@ use std::rc::Rc;
 /// Should-be-opaque type for the inner representation of CID.
 /// It has to be serializable and Borsh-serializable, as well as implement `Debug`, `Eq`, `Ord`, `Hash` and similar
 /// basic traits.  It is also can be unsized.
-// You should be able to replace it with [u8], and most of the code will just work.
+// you should be able to replace it with [u8], and most of the code will just work
 pub type CidRef = str;
+
+// there is no Rust multicodec crate with appropriate constants
+const JSON_CODEC: u64 = 0x0200;
 
 #[derive(Serialize, Deserialize)]
 #[serde(transparent)]
@@ -93,6 +100,16 @@ impl<Val> std::hash::Hash for CID<Val> {
     }
 }
 
+impl<T: ?Sized> std::convert::TryFrom<&'_ CID<T>> for cid::Cid {
+    type Error = cid::Error;
+
+    fn try_from(value: &CID<T>) -> Result<Self, Self::Error> {
+        use std::str::FromStr;
+
+        cid::Cid::from_str(&value.0)
+    }
+}
+
 // TODO we might refactor this to `SerializationFormat` trait
 // that both transform data to binary/text form (be it JSON, CBOR or something else)
 // and produces CID too
@@ -102,8 +119,6 @@ pub fn json_data_cid<Val: ?Sized>(data: &[u8]) -> CID<Val> {
 
     // the Sha2_256 is current IPFS default hash
     let digest = Code::Sha2_256.digest(data);
-    // seems to be better than RAW_CODEC = 0x55
-    const JSON_CODEC: u64 = 0x0200;
 
     let cid = Cid::new_v1(JSON_CODEC, digest);
     CID::new(cid.to_string())
@@ -144,18 +159,55 @@ pub fn value_to_json_cid<Val: Serialize + ?Sized>(
 ) -> Result<CID<Val>, CidCalculationError> {
     use cid::Cid;
     use multihash::{Code, MultihashDigest};
-    use sha2::Digest;
-
-    let mut hasher = sha2::Sha256::new();
-    serde_json::to_writer(BufWriter::with_capacity(8 * 1024, &mut hasher), value)?;
-    let hash = hasher.finalize();
+    let hash = value_json_hash::<sha2::Sha256, Val>(value)?;
 
     let digest = Code::Sha2_256
         .wrap(&hash)
         .expect("can't happend: incorrect hash length");
-    // seems to be better than RAW_CODEC = 0x55
-    const JSON_CODEC: u64 = 0x0200;
 
     let cid = Cid::new_v1(JSON_CODEC, digest);
     Ok(CID::new(cid.to_string()))
+}
+
+pub(crate) fn value_json_hash<D: digest::Digest + std::io::Write, Val: Serialize + ?Sized>(
+    value: &Val,
+) -> Result<Vec<u8>, serde_json::Error> {
+    let mut hasher = D::new();
+    serde_json::to_writer(BufWriter::with_capacity(8 * 1024, &mut hasher), value)?;
+    let hash = hasher.finalize();
+    Ok(hash.to_vec())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use serde_json::json;
+
+    #[test]
+    fn test_cid_sha2_256() {
+        assert_eq!(
+            value_to_json_cid(&json!("test")).unwrap(),
+            CID::new(
+                    "bagaaierajwlhumardpzj6dv2ahcerm3vyfrjwl7nahg7zq5o3eprwv6v3vpa"
+                )
+        );
+        assert_eq!(
+            value_to_json_cid(&json!([1, 2, 3])).unwrap(),
+            CID::new(
+                    "bagaaierauyk65lxcdxsrphpaqdpiymcszdnjaejyibv2ohbyyaziix35kt2a"
+                )
+        );
+        assert_eq!(
+            value_to_json_cid(&json!(1)).unwrap(),
+            CID::new(
+                    "bagaaieranodle477gt6odhllqbhp6wr7k5d23jhkuixr2soadzjn3n4hlnfq"
+                )
+        );
+        assert_eq!(
+            value_to_json_cid(&json!({"key": 42})).unwrap(),
+            CID::new(
+                    "bagaaierad7lci6475zdrps4h6fmcpmqyknz5z6bw6p6tmpjkfyueavqw4kaq"
+                )
+        );
+    }
 }
