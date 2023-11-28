bdf25ef2758abf5f116f3ef05c13d242bb32ac56
Ivan Boldyrev <ivan@fluence.one>
2023-11-28 18:43:37 +0100

    Fix big-data and long-data benches generation
    It was invariably JSON, now it uses a dedicated type.

--- junk/gen-bench-data/src/cid_benchmarking/mod.rs
+++ junk/gen-bench-data/src/cid_benchmarking/mod.rs
@@ -110,6 +110,8 @@ pub fn cid_benchmarking_data(
     peer_id: String,
     particle_id: &str,
 ) -> Vec<u8> {
+    use air_interpreter_sede::Format;
+
     let mut curr_data: PreCidInterpeterData = read_data(curr_data_filename);
     let calls: TraceCalls = read_data("src/cid_benchmarking/simple-calls-info.json");
     let mut calls = calls.into_iter();
@@ -159,7 +161,7 @@ pub fn cid_benchmarking_data(
         .unwrap()
         .insert("signatures".to_owned(), json!(ss));
 
-    serde_json::to_vec(&curr_data).unwrap()
+    InterpreterDataFormat::default().to_vec(&curr_data).unwrap().into()
 }
 
 pub fn cid_benchmarking_long_data(
