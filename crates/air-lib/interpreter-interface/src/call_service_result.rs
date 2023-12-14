b520ac65f3fbf7a69196ffdc7c888273b856f3ce
Ivan Boldyrev <ivan@fluence.one>
2023-12-14 14:36:02 +0100

    Use JSON for CallResults/CallRequests
    Somewhat compatible with the original data.

--- crates/air-lib/interpreter-interface/src/call_service_result.rs
+++ crates/air-lib/interpreter-interface/src/call_service_result.rs
@@ -16,7 +16,7 @@
 
 use air_interpreter_sede::define_simple_representation;
 use air_interpreter_sede::derive_serialized_type;
-use air_interpreter_sede::MsgPackMultiformat;
+use air_interpreter_sede::JsonFormat;
 use air_interpreter_sede::Representation;
 use serde::Deserialize;
 use serde::Serialize;
@@ -26,7 +26,7 @@ use std::collections::HashMap;
 pub type CallResults = HashMap<u32, CallServiceResult>;
 pub const CALL_SERVICE_SUCCESS: i32 = 0;
 
-pub type CallResultsFormat = MsgPackMultiformat;
+pub type CallResultsFormat = JsonFormat;
 
 derive_serialized_type!(SerializedCallResults);
 
