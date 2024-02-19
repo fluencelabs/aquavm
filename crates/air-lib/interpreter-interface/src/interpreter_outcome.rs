fed44b5b72b5a604c36f657640db7098d8969c69
drrtuy <roman@fluence.one>
2024-02-19 22:07:08 +0000

    fix(avm-server): this changes the order of limit triggering flags

--- crates/air-lib/interpreter-interface/src/interpreter_outcome.rs
+++ crates/air-lib/interpreter-interface/src/interpreter_outcome.rs
@@ -117,14 +117,15 @@ impl InterpreterOutcome {
             ));
         }
 
-        let air_size_limit_exceeded =
-            try_as_boolean(record_values.pop().unwrap(), "air_size_limit_exceeded")?;
-        let particle_size_limit_exceeded =
-            try_as_boolean(record_values.pop().unwrap(), "particle_size_limit_exceeded")?;
         let call_result_size_limit_exceeded = try_as_boolean(
             record_values.pop().unwrap(),
             "call_result_size_limit_exceeded",
         )?;
+        let particle_size_limit_exceeded =
+            try_as_boolean(record_values.pop().unwrap(), "particle_size_limit_exceeded")?;
+        let air_size_limit_exceeded =
+            try_as_boolean(record_values.pop().unwrap(), "air_size_limit_exceeded")?;
+
         let call_requests = try_as_byte_vec(record_values.pop().unwrap(), "call_requests")?;
         let next_peer_pks = try_as_string_vec(record_values.pop().unwrap(), "next_peer_pks")?;
         let data = try_as_byte_vec(record_values.pop().unwrap(), "data")?;
